7e2751acec56997d495251178d464be1ee6da441
ali ansari <aliansari685@outlook.com>
2025-10-26 00:29:18 +0330

    Add project files.

---
+++ DynamicReporting.Api/Domain/Orders.cs
@@ -0,0 +1,8 @@
+﻿namespace DynamicReporting.Api.Domain;
+
+public class Orders
+{
+    //todo : using flunet and create full model - rename db - sizable tables 
+    [Required]
+    public int Id { get; set; }
+}
