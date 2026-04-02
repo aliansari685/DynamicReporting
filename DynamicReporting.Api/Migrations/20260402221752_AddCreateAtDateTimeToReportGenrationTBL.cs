41a6eb882e88c0063548a327142575173f722838
ali ansari <aliansari685@outlook.com>
2026-04-03 02:00:43 +0330

    AddCreateAtDateTimeToReportGenrationTBL Migration

---
+++ DynamicReporting.Api/Migrations/20260402221752_AddCreateAtDateTimeToReportGenrationTBL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DynamicReporting.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreateAtDateTimeToReportGenrationTBL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpDateTime",
+                table: "ReportGenerations",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "ReportGenerations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "ReportGenerations");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpDateTime",
+                table: "ReportGenerations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
