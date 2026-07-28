ff6cee8b20cd0aae00b41c402b0d50fbba1ae449
Ali Ansari <aliansari685@gmail.com>
2026-07-28 18:04:04 +0330

    .

---
+++ DynamicReporting.Api/Infrastructure/Services/PdfExportService.cs
@@ -0,0 +1,30 @@
+﻿namespace DynamicReporting.Api.Infrastructure.Services;
+
+//todo :
+public class PdfExportService(
+    IReportDataService reportDataService,
+    IReportExportService exportService,
+    IReportMetadataService metadataService,
+    IReportQueryBuilder reportQueryBuilder) : IExportService
+{
+    public Task<bool> ExportAsync(int reportDefinitionId, List<FilterCondition>? filtersList, Stream outputStream,
+        SortableColumnDto sortColumn, CancellationToken cancellationToken = default)
+    {
+        {
+            return Task.FromResult(true);
+        }
+    }
+
+    public Task ExportWithAutoFitColumnsAsync(int reportDefinitionId, List<FilterCondition>? filtersList, Stream outputStream,
+        SortableColumnDto sortColumn, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception exception)
+        {
+            return Task.FromException(exception);
+        }
+    }
+}
