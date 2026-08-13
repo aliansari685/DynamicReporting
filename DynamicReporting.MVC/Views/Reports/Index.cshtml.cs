285cea30a3643cff919b8fbb81ecf962fd3907e4
ali ansari <aliansari685@outlook.com>
2026-08-13 05:16:17 +0330

    Add Reprots View

---
+++ DynamicReporting.MVC/Views/Reports/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DynamicReporting.Mvc.Views.Reports
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
