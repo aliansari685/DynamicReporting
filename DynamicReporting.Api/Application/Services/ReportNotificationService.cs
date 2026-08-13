ca3cc29da6d04bd69af5ab0778ebb1e1f041f3fe
ali ansari <aliansari685@outlook.com>
2026-08-13 05:13:21 +0330

    Add ReportGuid To SendNotid

--- DynamicReporting.Api/Application/Services/ReportNotificationService.cs
+++ DynamicReporting.Api/Application/Services/ReportNotificationService.cs
@@ -4,10 +4,12 @@ public class ReportNotificationService(IHubContext<ReportHub> hubContext) : IRep
 {
     public async Task NotifyReportReadyAsync(Guid reportGuid)
     {
-        await hubContext.Clients.Group(reportGuid.ToString() /*data or url link or everything*/)
+        await hubContext.Clients
+            .Group(reportGuid.ToString())
             .SendAsync("ReportReady", new
             {
-                message = "گزارش شما آماده است برای دانلود به صفحه ی گزارشات مراجعه کنید"
+                reportGuid,
+                message = "گزارش شما آماده است برای دانلود به صفحه گزارشات مراجعه کنید."
             });
     }
 }
